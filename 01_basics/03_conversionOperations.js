99f462514f92db299413d392bea51df55e1cfc41
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-10-04 14:26:08 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "true";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valInNumber = Number(score);
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// null => 0;
+// undefined => NaN
+
+let isLoggedIn = "qutub";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false;
+// "qutub" => true;
+
+let someNumber = 33;
+let stringConverNumber = String(someNumber)
+console.log(stringConverNumber)
+console.log(typeof stringConverNumber)
