bfb047bf23c9fbddb0ab1d9f4a885e26b1b016ed
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-10-04 18:06:48 +0000

    learn about comaprisons

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let stringConverNumber = String(someNumber)
-console.log(stringConverNumber)
-console.log(typeof stringConverNumber)
+// console.log(stringConverNumber)
+// console.log(typeof stringConverNumber)
+
+
+
+// **************************** Operations ******************************* // 
+
+let value =3 ;
+let negValue = -value;
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // to make an exponent of number in javascript
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1= "Hello"
+let str2 = " qutub"
+// console.log(str1 + str2) // concatenation
+
+// console.log("1" + 2); 
+// console.log(typeof 1 + 2 + "2" + 3 + "4"); // log(3234) type is number without brackets
+// console.log(typeof (1 + 2 + "2" + 3 + "4")); // log(3234) type is string with brackets
+// console.log(+"")
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num2) 
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
