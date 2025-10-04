5700ff8c7184c343b4e4d5ad8538fc01418b6363
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-10-04 12:48:14 +0000

    variables keywords

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144533;
+let accountEmail = "hitesh@google.com"
+let accountPassword = "12345"
+accountCity = "karachi"
+
+// accountId = 2;
+
+accountEmail = "qutub@google.com"
+accountPassword = "909090"
+accountCity = "lahore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
